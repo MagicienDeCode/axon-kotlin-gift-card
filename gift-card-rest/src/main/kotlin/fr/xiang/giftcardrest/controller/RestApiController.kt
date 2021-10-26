69539fa4c3e1c33d5f008990a915fe47ce6065a7
Xiang LI <xli@slimpay.com>
2021-10-26 10:38:06 +0200

    Add read events

--- gift-card-rest/src/main/kotlin/fr/xiang/giftcardrest/controller/RestApiController.kt
+++ gift-card-rest/src/main/kotlin/fr/xiang/giftcardrest/controller/RestApiController.kt
@@ -5,6 +5,7 @@ import fr.xiang.giftcardapi.command.IssueCommand
 import fr.xiang.giftcardapi.command.RedeemCommand
 import fr.xiang.giftcardapi.query.GetGiftCardQuery
 import fr.xiang.giftcardapi.query.model.GiftCardView
+import org.axonframework.eventsourcing.eventstore.EventStore
 import org.axonframework.extensions.reactor.commandhandling.gateway.ReactorCommandGateway
 import org.axonframework.extensions.reactor.queryhandling.gateway.ReactorQueryGateway
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import kotlin.streams.toList
 
 @RestController
 @RequestMapping("/card/")
 class RestApiController(
     private val commandGateway: ReactorCommandGateway,
-    private val queryGateway: ReactorQueryGateway
+    private val queryGateway: ReactorQueryGateway,
+    private val eventStore: EventStore
 ) {
 
     @GetMapping("create/{id}/{amount}")
@@ -35,4 +38,9 @@ class RestApiController(
     @GetMapping("{id}")
     fun getCard(@PathVariable id: String): Mono<GiftCardView> =
         queryGateway.query(GetGiftCardQuery(id), GiftCardView::class.java)
+
+    @GetMapping("{id}/events")
+    fun getEvents(@PathVariable id: String) =
+        eventStore.readEvents(id).asStream().map { it.toString() }.toList()
+
 }
